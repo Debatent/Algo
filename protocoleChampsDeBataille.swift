4cd828083f94038c44d91d6e8d2eba0ca2a59dc5
Debatent <alexandreadrien@orange.fr>
2018-12-04 19:43:51 +0100

    ajout afficherposition dans champ de bataille

--- protocoleChampsDeBataille.swift
+++ protocoleChampsDeBataille.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 protocol ProtocoleChampsDeBataille: Sequence {
-    associatedtype IteratorChampsDeBataille : IteratorProtocol where IteratorChampsDeBataille.Element == ProtocolePosition 
+    associatedtype IteratorChampsDeBataille : IteratorProtocol where IteratorChampsDeBataille.Element == ProtocolePosition
 
 
     // Une position du champs de bataille
-    var position: [ProtocolePosition] { get set } 
+    var position: [ProtocolePosition] { get set }
 
     // init: -> ChampsDeBataille
     // Création du champs de bataille avec 6 positions: 3 Fronts et 3 Arrières (vide)
@@ -18,9 +18,9 @@ protocol ProtocoleChampsDeBataille: Sequence {
     mutating func sortir(p: ProtocolePosition) -> ProtocoleCarte
 
     // poser: ChampsDeBataille x Carte x Position
-    // Ajoute une carte au champs de bataille à partir d'une position, une carte ne peut être posé à l'arrière à moins 
+    // Ajoute une carte au champs de bataille à partir d'une position, une carte ne peut être posé à l'arrière à moins
     // qu'il y ait une carte posée au front correspondant, dans le cas échéant, la carte est posée au front. Si une carte est déjà présente à la position où
-    // 
+    //
     // Pre: Carte qui correspond à la carte que l'on veut poser & Position qui correspond à la position à laquelle on veut poser la carte
     mutating func poser(c: ProtocoleCarte, p: ProtocolePosition) throws
 
@@ -28,6 +28,10 @@ protocol ProtocoleChampsDeBataille: Sequence {
     // Déplace une carte présente sur le champs de bataille d'une position à une autre
     // Pre: Position de départ p1 correspondant à la position où la carte est placée &  position de fin p2 correspondant à la position où l'on veut placer la carte
     mutating func deplacer(p1: ProtocolePosition, p2: ProtocolePosition)
+
+    //renvoie la position du champs de bataille lié à ces coordonnés
+    //Pre: 1<=colonne<=3
+    func afficherposition(_front: Bool, _colonne: Int)->ProtocolePosition
 
     // getCarte: -> Bool
     // Regarde si il y a une carte à cette position
@@ -41,7 +45,7 @@ protocol ProtocoleChampsDeBataille: Sequence {
     func estAportee(p1: ProtocolePosition, C2: ProtocoleChampsDeBataille, p2: ProtocolePosition) -> Bool
 
    // makeIterator : ChampsDeBataille -> ItChampsDeBataille
-   // crée un itérateur sur la collection pour la parcourir dans l'ordre croissant des positions du front  
+   // crée un itérateur sur la collection pour la parcourir dans l'ordre croissant des positions du front
     func makeItFront() -> IteratorChampsDeBataille
 
 
