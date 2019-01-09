6361f3840a1e625627eb64109e57edbf693074f2
Invité <guest-gfmgnz@igtp5-pc06.isim.intra>
2019-01-09 08:50:00 +0100

    Correction de bug de Position

--- Projet_Algo_Dupre_Trannoy/Library_Dupre_Trannoy/Sources/Library_Dupre_Trannoy/position.swift
+++ Projet_Algo_Dupre_Trannoy/Library_Dupre_Trannoy/Sources/Library_Dupre_Trannoy/position.swift
@@ -7,7 +7,7 @@ public protocol PositionProtocol{
 
     //init : -> Position
     //une position est soit un charactere alphabetique correspondant a une case sur le terrain ("a","b","c","d","e","f","g","h","i","j","k","l") (de "a" à "f", c'est le joueur 1 et de de "g" à "l" ,c'est le joueur 2) (avec "a" en bas à gauche et "l" en haut à droite)soit a vide si la piece est dans la reserve
-    init(pos : (Character?))
+    init(_ pos : (Character?))
 
 	// getposcharacter: Position -> Character?
 	// permet d'obtenir la lettre de cette position
@@ -25,7 +25,7 @@ public struct Position : PositionProtocol{
 
     //init : -> Position
     //une position est soit un charactere alphabetique correspondant a une case sur le terrain ("a","b","c","d","e","f","g","h","i","j","k","l") (de "a" à "f", c'est le joueur 1 et de de "g" à "l" ,c'est le joueur 2) (avec "a" en bas à gauche et "l" en haut à droite)soit a vide si la piece est dans la reserve
-    init(_ pos : (Character?)) {
+    public init(_ pos : (Character?)) {
         switch pos {
             case "a":
                 self.char = "a"
