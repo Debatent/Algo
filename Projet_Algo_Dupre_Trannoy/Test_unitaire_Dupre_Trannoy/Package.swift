f45117b4f80b9ca3a768f343688a8f53ec3b5b24
Arthur Leblanc <arthurleblanc11@gmail.com>
2019-01-14 15:37:07 +0100

    Ajout dependencies

--- Projet_Algo_Dupre_Trannoy/Test_unitaire_Dupre_Trannoy/Package.swift
+++ Projet_Algo_Dupre_Trannoy/Test_unitaire_Dupre_Trannoy/Package.swift
@@ -5,16 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "Test_unitaire_Dupre_Trannoy",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Test_unitaire_Dupre_Trannoy",
+            targets: ["Test_unitaire_Dupre_Trannoy"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(path: "../Library_Dupre_Trannoy")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Test_unitaire_Dupre_Trannoy",
-            dependencies: []),
+            dependencies: ["Library_Dupre_Trannoy"]),
         .testTarget(
             name: "Test_unitaire_Dupre_TrannoyTests",
             dependencies: ["Test_unitaire_Dupre_Trannoy"]),
